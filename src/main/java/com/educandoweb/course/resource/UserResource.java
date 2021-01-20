7c2cbcc4245fba90309b036f86fc48be9e93a7b8
Cristiano Pontes <pontescr@gmail.com>
2021-01-19 23:03:08 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/educandoweb/course/resource/UserResource.java
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -1,27 +1,26 @@
 package com.educandoweb.course.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.domain.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		List<User> list = new ArrayList<>();
-		User lanna = new User("1", "Lanna Beatriz", "lanna@gmail.com");
-		User laura = new User("2", "Laura Pontes", "laura@gmail.com");
-		list.addAll(Arrays.asList(lanna, laura));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
